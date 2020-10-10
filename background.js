ac29d831b2ea4d0f8ebb48f47a2d8100300048d8
saikat <sguha@salesforce.com>
2020-10-11 02:26:20 +0530

    add menu item to context menu also

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+chrome.contextMenus.create({
+    title: "Open New Tab",
+    onclick: () => chrome.tabs.create({})
+});
 chrome.commands.onCommand.addListener(function (command) {
     if (command === "openNewTab") {
         chrome.tabs.create({});
