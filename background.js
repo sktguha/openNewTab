dac3d6ed212fd7c58c1cd737dad7395d09b5ee16
saikat <sguha@salesforce.com>
2020-10-13 12:46:59 +0530

    add support for different popular websites thus saving extra click

--- background.js
+++ background.js
@@ -1,7 +1,27 @@
 chrome.contextMenus.create({
     title: "Open New Tab",
+    id: "parent",
     onclick: () => chrome.tabs.create({})
 });
+const openUrl = (url) => chrome.tabs.create({ url });
+const URLS = [
+    "https://www.facebook.com/",
+    "https://www.linkedin.com/",
+    "https://www.quora.com/",
+    "https://www.github.com/",
+    "https://www.instagram.com/",
+    "https://twitter.com/",
+    "https://news.ycombinator.com/",
+    "https://gmail.com/",
+    "https://www.reddit.com/",
+    "https://github.com/"
+];
+URLS.forEach(url => chrome.contextMenus.create({
+    title: "Open " + url,
+    parentId: "parent",
+    onclick: openUrl.bind(this, url)
+}));
+
 chrome.commands.onCommand.addListener(function (command) {
     if (command === "openNewTab") {
         chrome.tabs.create({});
